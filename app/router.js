518a1d51fef48e41a60720531dec705728d06daf
Daan Middendorp <github-d@landgenoot.com>
2017-11-03 23:25:24 +0100

    Structure of tripfinder update

--- app/router.js
+++ app/router.js
@@ -1,7 +1,12 @@
 const router = require('koa-router')()
+const tripfinder = require('../lib/tripfinder.js')
 
 router.get('/', async ctx => {
-  ctx.body = 'Airtrip finder running'
+  ctx.body = 'Airtrip finder API running'
+})
+
+router.get('/api/tripfinder', async ctx => {
+  ctx.body = await tripfinder()
 })
 
 module.exports = router
